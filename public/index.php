862854b63a95d7040003389caf4f10a967c68d74
Carlos <calososla@gmail.com>
2024-10-11 20:33:42 -0300

    Adição de arquivos iniciais e pastas

---
+++ public/index.php
@@ -0,0 +1,30 @@
+<?php
+
+    require_once'src/db.php';
+    $conn = conectarBanco()
+
+?>
+
+
+<!DOCTYPE HTML>
+<html lang = "PT-BR">
+
+<head>
+
+    <meta charset = "UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Avaliação de Serviços Prestados pelo Hospital Regial do Alto Valedo Itajaí - HRAV </title>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <h1>Sistema de Avaliação</h1>
+
+    <form action = ">
+
+    </form>
+
+
+</body>
